c4da138b3d3062bb10e274b479dc3795b233c12d
Sohini Bhattacharya <bhattacharyasohini48@gmail.com>
2023-01-29 19:34:26 +0530

    Category Controller Added

--- Expense Tracker/Models/ApplicationDbContext.cs
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -5,10 +5,9 @@ namespace Expense_Tracker.Models
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
+        { 
 
         }
-
 
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Category> Categories { get; set; }
